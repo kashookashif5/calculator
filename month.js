dde8f4cfcd7d58e6a58522e17a7eef40751075b5
kashookashif5 <kashookashif5@gmail.com>
2024-06-05 21:35:44 +0500

    new repo

---
+++ month.js
@@ -0,0 +1,22 @@
+const firstvalue =+prompt("enter the first number") 
+const operators =prompt("oparetors +.- ./ .*")
+const secondvalue =+prompt("enter the secondvalue")
+
+let calvale=0;
+
+if(operator== "+"){
+    calvale=("firstvalue + secondvalue")
+}
+else if(operator =="-"){
+    calvale=("firstvalue-secondvalue")
+}
+else if(operator =="*"){
+    calvale("firstvale * secondvalue")
+}
+else if(operator =="/"){
+        calvale("firstvale / secondvalue")
+} 
+else{
+    calvale(no)    
+}
+    
